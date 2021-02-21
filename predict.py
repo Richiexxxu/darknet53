46ce4c8fcc59841e7a755682e68b3487143297dd
Richie <776448034@qq.com>
2021-02-21 16:58:18 -0500

    2.21.lab-2

--- predict.py
+++ predict.py
@@ -1,6 +1,30 @@
+import os
+import json
 import torch
+
+from PIL import Image
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
 from model import darknet53
 
-checkpoint = torch.load("model_best.pth.tar", map_location='cpu')
-model = darknet53(num_classes=1000)
-model.load_state_dict(checkpoint['state_dict'])
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.106],
+                                     std=[0.229, 0.224, 0.225])
+
+    data_transform = transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            normalize])
+
+    # load image
+
+    # create model
+
+
+
