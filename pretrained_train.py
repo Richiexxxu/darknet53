f1fbc30295ab82a6cda79647e565d5d8d61deca5
Richie <776448034@qq.com>
2021-02-27 17:20:02 -0500

    '2021-02-27'

--- pretrained_train.py
+++ pretrained_train.py
@@ -16,7 +16,7 @@ from model import darknet53
 parser = argparse.ArgumentParser(description='Darknet53 ImageNet Training')
 parser.add_argument('--data', default='dataset',metavar='DIR', help='path to datset')
 parser.add_argument('--resume', default='', type=str, metavar='Path', help='path to latest checkpoint (default:none)')
-parser.add_argument('--epochs', default=90, type=int, metavar='N', help='number of total epochs to run')
+parser.add_argument('--epochs', default=30, type=int, metavar='N', help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N', help='manual epoch number (useful on restarts)')
 parser.add_argument('--workers', default=4, type=int, metavar='N', help='number of data loading workers （default:4)')
 parser.add_argument('--lr', '--learning-rate', default=0.1, type=float, metavar='LR', help='initial learning rate', dest='lr')
@@ -24,11 +24,11 @@ parser.add_argument('--momentum', default=0.9, type=float, metavar='M', help='mo
 parser.add_argument('--weight-decay', default=1e-4, type=float,
                     metavar='W', help='weight decay (default: 1e -4)',
                     dest='weight_decay')
-parser.add_argument('--batch-size', default=32, type=int, metavar='N',
+parser.add_argument('--batch-size', default=16, type=int, metavar='N',
                     help='mini-batch size (default;32), this is the total'
                          'batch size of all GPUs on the current node when '
                          'using Data Parallel or Distributed Data Parallel')
-parser.add_argument('--print-freq', default=10, type=int,
+parser.add_argument('--print-freq', default=100, type=int,
                     metavar='N', help='print frequency (defaule : 10)')
 parser.add_argument('--gpu', default=0, type=int, help='GPU id to use.')
 # parser.add_argument('--nclasses')
@@ -87,9 +87,10 @@ def main():
         ]))
 
     # generate label  .txt
-    train_num = len(train_dataset)
+
     cla_list = train_dataset.class_to_idx
     cla_dict = dict((val, key) for key, val in cla_list.items())
+    train_num = len(cla_dict)
     # write dict into json file
     json_str = json.dumps(cla_dict, indent=4)
     with open('class_indices.json', 'w') as json_file:
@@ -105,7 +106,9 @@ def main():
 
     # change fc layer structure
     in_channel = model.fc.in_features
+    print(train_num)
     model.fc = nn.Linear(in_channel, train_num)
+    print(model.fc)
     # model.cuda(args.gpu)
     torch.cuda.set_device(args.gpu)
     model.cuda(args.gpu)
@@ -175,7 +178,7 @@ def train(train_loader, model ,criterion, optimizer, epoch, args):
     model.train()
 
     end = time.time()
-
+    running_loss = 0.0
     for i, (input, target) in enumerate(train_loader):
         # measure data loading time
         data_time.update(time.time() - end)
@@ -191,7 +194,7 @@ def train(train_loader, model ,criterion, optimizer, epoch, args):
 
         # measure accuracy and record loss
         acc1, acc5 = accuracy(output, target, topk=(1, 5))
-        losses.update(acc1[0], input.size(0))
+        losses.update(loss.item(), input.size(0))
         top1.update(acc1[0], input.size(0))
         top5.update(acc5[0], input.size(0))
 
@@ -203,16 +206,20 @@ def train(train_loader, model ,criterion, optimizer, epoch, args):
         # measure elapsed time
         batch_time.update(time.time() - end)
         end = time.time()
-
+        running_loss += loss.item()
         if i % args.print_freq ==0:
+
             print('Epoch: [{0}][{1}/{2}]\t'
                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                   'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                    'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
                   'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
                   'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
                 epoch, i, len(train_loader), batch_time=batch_time,
                 data_time=data_time, loss=losses, top1=top1, top5=top5))
+            print("loss: ", loss)
+
+
 
 def accuracy(output, target, topk = (1,)):
     """ Computes the accuracy over the k top predictions for the specified values of k """
